88e9f45ad8b25d6dd2fa3418bffaec1fb0191c35
LuciaPelaez <lucygeo@gmail.com>
2026-03-05 09:08:25 +0100

    feat/add_file

---
+++ JUEGO PAR O IMPAR.py
@@ -0,0 +1,10 @@
+#"JUEGO ESCRIBE UN NÚMERO, Y YO ADIVINARÉ SI ES PAR O IMPAR"
+
+print("Bienvenido! Escribe un número y yo adivinaré si es par o impar") 
+
+intento = int(input("Introduce un numero: "))
+
+if intento % 2 == 0:
+    print ("ES UN NUMERO PAR")
+else:
+    print ("ES UN NUMERO IMPAR")
